d5188fb892ddcdff4d12df16ee96c690590ba996
ItsMePaz <michael.paz@pulsar-sd.com>
2024-05-22 19:34:36 +0800

    feat: added components, routing, and initialized firebase features
    -firebase features included: firestore, authentication, and hosting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+// landing page component
+import { LandingComponent } from './components/landing/landing.component';
+// admin components
+import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
+import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
+// customer components
+import { CustomerDashboardComponent } from './components/customer/customer-dashboard/customer-dashboard.component';
+import { CustomerLoginComponent } from './components/customer/customer-login/customer-login.component';
+import { CustomerRegistrationComponent } from './components/customer/customer-registration/customer-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
+  { path: 'admin/dashboard', component: AdminDashboardComponent },
+  { path: 'admin/login', component: AdminLoginComponent },
+  { path: 'customer/dashboard', component: CustomerDashboardComponent },
+  { path: 'customer/login', component: CustomerLoginComponent },
+  { path: 'customer/registration', component: CustomerRegistrationComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
+import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
+import { CustomerDashboardComponent } from './components/customer/customer-dashboard/customer-dashboard.component';
+import { CustomerLoginComponent } from './components/customer/customer-login/customer-login.component';
+import { CustomerRegistrationComponent } from './components/customer/customer-registration/customer-registration.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+/* import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
+import { CustomerLoginComponent } from './components/customer/customer-login/customer-login.component';
+import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
+import { CustomerDashboardComponent } from './components/customer/customer-dashboard/customer-dashboard.component';
+import { CustomerRegistrationComponent } from './components/customer/customer-registration/customer-registration.component'; */
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AdminLoginComponent,
+    CustomerLoginComponent,
+    AdminDashboardComponent,
+    CustomerDashboardComponent,
+    CustomerRegistrationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
