4a814daea9036321f10fef698040592a1155121c
ItsMePaz <michael.paz@pulsar-sd.com>
2024-05-23 06:14:16 +0800

    feat: added admin and customer login components

--- src/app/components/customer/customer-login/customer-login.component.ts
+++ src/app/components/customer/customer-login/customer-login.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-customer-login',
   templateUrl: './customer-login.component.html',
-  styleUrls: ['./customer-login.component.css']
+  styleUrls: ['./customer-login.component.css'],
 })
 export class CustomerLoginComponent {
+  private subscription: Subscription | undefined;
+  email: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    if (this.email == '') {
+      alert('Please enter email');
+      return;
+    }
+
+    if (this.password == '') {
+      alert('Please enter password');
+      return;
+    }
+
+    // Here we subscribe to the adminLogin function that returns an observable
+    // (ergo, we can subscribe to it)
+    // then it starts emitting values
+    this.subscription = this.authService
+      .userLogin(this.email, this.password, 'customer')
+      .subscribe({
+        next: (value: any) => {
+          const isAdmin = value['isAdmin'];
+          if (isAdmin || Object.keys(value).length === 0) {
+            alert('Not a customer account!');
+            this.router.navigate(['customer/login']);
+          } else {
+            const res = value['res'];
+            const tokenResponse = res['_tokenResponse'];
+            const userEmail = value['userEmail'];
+            localStorage.setItem('idToken', tokenResponse['idToken']);
+            localStorage.setItem('userEmail', userEmail);
+            localStorage.setItem('isAdmin', isAdmin);
+            this.router.navigate(['customer/dashboard']);
+          }
+        },
+        error: (err: string) => {
+          alert('Error logging in: ' + err);
+          this.router.navigate(['customer/login']);
+        },
+      });
+
+    this.email = '';
+    this.password = '';
+  }
+
+  // To ensure that the subscription is cleaned up when the component is destroyed.
+  // Preventing memory leaks
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
