38e2456992695655334680d7ce58e8c8b84a8029
ItsMePaz <michael.paz@pulsar-sd.com>
2024-05-31 18:45:35 +0800

    update: polished template styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
+  /*   theme: {
+    extend: {},
+  }, */
   plugins: [],
 };
