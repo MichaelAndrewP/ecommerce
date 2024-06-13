105704f760c24d9cd957ae01d0a05b4a9ab833eb
ItsMePaz <michael.paz@pulsar-sd.com>
2024-06-13 14:53:21 +0800

    feat: added nodemailer

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,26 +1,74 @@
 import * as admin from 'firebase-admin';
 import { onDocumentUpdated } from 'firebase-functions/v2/firestore';
+import * as nodemailer from 'nodemailer';
+import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'mpazdev@gmail.com',
+    pass: 'gqqrkpdrfqufroca',
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+const sendEmail = async (email: string, subject: string, text: string) => {
+  const mailOptions = {
+    from: 'mpazdev@gmail.com',
+    to: email,
+    subject: subject,
+    text: text,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Welcome email sent successfully!');
+  } catch (error) {
+    console.log('Error sending email: ', error);
+  }
+};
+
+const assignRole = async (email: string, role: string) => {};
+
+export const createCustomer = functions.auth.user().onCreate(async (user) => {
+  try {
+    await sendEmail(
+      user.email as string,
+      'Welcome to my practice project!',
+      'Click here to get started: http://localhost:4200/customer/login'
+    );
+  } catch (error) {
+    console.log('Error sending email: ', error);
+  }
+});
 export const updateProduct = onDocumentUpdated(
   'products/{productId}',
   async (event: { data: any }) => {
     try {
-      const data = event.data.after.data();
-
-      // Check if a product with the same name already exists
-      const productsRef = admin.firestore().collection('products');
-
-      const snapshot = await productsRef.where('name', '==', data.name).get();
-      if (!snapshot.empty) {
-        console.log('Error: Product with this name already exists');
-        return;
-      }
-
-      console.log('Product created successfully');
+      await sendEmail(
+        'pazmichaelandrew70@gmail.com',
+        'test',
+        'This is a test from mpazdev'
+      );
     } catch (error) {
-      console.log('Error creating product: ', error);
+      console.log('Error sending email: ', error);
     }
   }
 );
+
+export const assginUserRole = functions.auth.user().onCreate(async (user) => {
+  try {
+    console.log('User: ', user);
+    await admin.firestore().collection('customer').doc(user.uid).set({
+      email: user.email,
+      role: 'customer',
+    });
+    console.log(`User: ${user.email} assigned role: customer`);
+  } catch (error) {
+    console.log('Error assigning user role: ', error);
+  }
+});
