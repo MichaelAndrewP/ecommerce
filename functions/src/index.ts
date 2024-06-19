c05c5ca6c6c08a0622db17c2cd95d7d01af1f29a
Michael Paz <michael.paz@pulsar-sd.com>
2024-06-19 10:55:51 +0800

    update: updated packages and configs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,44 @@ import * as admin from 'firebase-admin';
 import { onDocumentUpdated } from 'firebase-functions/v2/firestore';
 import * as nodemailer from 'nodemailer';
 import * as functions from 'firebase-functions';
+import * as pubsub from '@google-cloud/pubsub';
 
 admin.initializeApp();
+
+process.env.PUBSUB_EMULATOR_HOST = 'localhost:8085';
+
+const pubSubClient = new pubsub.PubSub();
+
+async function listSubscriptions() {
+  try {
+    const [subscriptions] = await pubSubClient
+      .topic('products')
+      .getSubscriptions();
+    console.log('Subscriptions:');
+    subscriptions.forEach((subscription) => console.log(subscription.name));
+  } catch (error) {
+    console.log('Error listing subscriptions: ', error);
+  }
+}
+
+export const createProductTopic = functions.https.onRequest(
+  async (req, res) => {
+    const topicName = 'products';
+
+    try {
+      await pubSubClient.createTopic(topicName);
+      console.log(`Topic ${topicName} created.`);
+
+      // Call listSubscriptions function after the topic is created
+      await listSubscriptions();
+
+      res.status(200).send(`Topic ${topicName} created.`);
+    } catch (error) {
+      console.error(`Error creating topic ${topicName}: `, error);
+      res.status(500).send(`Error creating topic ${topicName}: ${error}`);
+    }
+  }
+);
 
 let transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -32,7 +68,7 @@ const sendEmail = async (email: string, subject: string, text: string) => {
   }
 };
 
-const assignRole = async (email: string, role: string) => {};
+/* const assignRole = async (email: string, role: string) => {}; */
 
 export const createCustomer = functions.auth.user().onCreate(async (user) => {
   try {
@@ -71,3 +107,21 @@ export const assginUserRole = functions.auth.user().onCreate(async (user) => {
     console.log('Error assigning user role: ', error);
   }
 });
+
+export const publishMessage = functions.firestore
+  .document('products/{productId}')
+  .onCreate(async (snap, context) => {
+    try {
+      const message = snap.data();
+      const dataBuffer = Buffer.from(JSON.stringify(message));
+      const messageId = await pubSubClient
+        .topic('products')
+        .publishMessage({ data: dataBuffer });
+      await listSubscriptions();
+      console.log(`Message published.`, messageId);
+      return null;
+    } catch (error) {
+      console.log('Error publishing message: ', error);
+      return null;
+    }
+  });
